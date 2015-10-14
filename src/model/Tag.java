9706baa488746ce77795ea4433d20da2910f1814
Julian Tobias Rosemann <s9jnrose@stud.uni-saarland.de>
2015-10-14 14:57:18 +0200

    impl Task : getter, setter, start, stop, finish

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -6,9 +6,33 @@
 
 package model;
 
+import java.util.Objects;
+
 
 public class Tag extends Tree<Tag> {
 
 	private String descr;
+
+	@Override
+	public int hashCode() {
+		int hash = 3;
+		hash = 59 * hash + Objects.hashCode(this.descr);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Tag other = (Tag) obj;
+		if (!Objects.equals(this.descr, other.descr)) {
+			return false;
+		}
+		return true;
+	}
 
 }
--- src/model/Task.java
+++ src/model/Task.java
@@ -11,23 +11,19 @@ import java.util.Calendar;
 
 public class Task extends Tree<Task> {
 
-	/*
-	context conditions:
-
-		FRAGMENT <=> at least one of the essential properties is unset
-
-		RECEIVABLE => actDur = 0
-
-	*/
 
 	public Task() {
+		fragment = true;
+		active = false;
+		done = false;
 		importance = -1;
 		urgency = -1;
+		mngmnt = false;
 	}
 
 	//status:
 	private boolean fragment;
-	private Activity activitiy;
+	private boolean active;
 	private boolean done;
 
 	private boolean mngmnt;
@@ -41,22 +37,105 @@ public class Task extends Tree<Task> {
 	private Calendar expires;
 
 	//variable
-	private int actDur;
+	private long actDur;
+
+	private long starttime;
+
+	public void start() {
+		if (active) {
+			throw new IllegalStateException("already active");
+		}
+		starttime = System.currentTimeMillis();
+		active = true;
+	}
+
+	private void stop() {
+		if (!active) {
+			throw new IllegalStateException("already non-active");
+		}
+		actDur += System.currentTimeMillis() - starttime;
+		active = false;
+	}
+
+	public void finish() {
+		if (active) {
+			stop();
+		}
+		done = true;
+	}
+
+	public boolean isFragment() {
+		return fragment;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public long getActDur() {
+		return actDur;
+	}
+
 
 	void checkCompleteness() {
 		fragment
 			= descr == null || tag == null || importance == -1 || urgency == -1;
 	}
 
-	boolean hasTag(Tag tag) {
-		return this.tag == tag;
+	public boolean isMngmnt() {
+		return mngmnt;
 	}
 
-}
+	public void setMngmnt(boolean mngmnt) {
+		this.mngmnt = mngmnt;
+	}
 
-enum Activity {
-	BEFORE,
-	ACTIVE,
-	PAUSED,
-	STOPPED;
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+		checkCompleteness();
+	}
+
+	public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+		checkCompleteness();
+	}
+
+	public int getImportance() {
+		return importance;
+	}
+
+	public void setImportance(int importance) {
+		this.importance = importance;
+		checkCompleteness();
+	}
+
+	public int getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(int urgency) {
+		this.urgency = urgency;
+		checkCompleteness();
+	}
+
+	public Calendar getExpires() {
+		return expires;
+	}
+
+	public void setExpires(Calendar expires) {
+		this.expires = expires;
+	}
+
 }
