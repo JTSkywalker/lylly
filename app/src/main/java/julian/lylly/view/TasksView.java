c3148d5eca22819650cd2bfee0ac21b478f92334
Julian Tobias Rosemann <s9jnrose@stud.uni-saarland.de>
2015-11-28 09:19:07 +0100

    add TasksView with impl of TaskListView and also task_list_item

---
+++ app/src/main/java/julian/lylly/view/TasksView.java
@@ -0,0 +1,66 @@
+package julian.lylly.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.joda.time.Duration;
+
+import java.util.ArrayList;
+
+import julian.lylly.R;
+import julian.lylly.model.Tag;
+import julian.lylly.model.Task;
+
+/**
+ * Created by VAIO on 28.11.2015.
+ */
+public class TasksView {
+
+    private final MainActivity main;
+
+    //private final ListView budgetListView;
+    //private final ArrayAdapter budgetAdapter;
+
+    private final ListView taskListView;
+    private final ArrayAdapter taskAdapter;
+
+    public TasksView(final MainActivity main) {
+        this.main = main;
+        taskAdapter =
+                new ArrayAdapter<Task>(main, R.layout.task_list_item,
+                    main.getOrganizer().getFilteredTasks(new ArrayList<Tag>())) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                Task task = getItem(position);
+                boolean active = task.isActive();
+                Duration timer = task.evalDurationSum();
+                String descr   = task.getDescr();
+                Tag tag        = task.getTag();
+                if (convertView == null) {
+                    LayoutInflater inflater = main.getLayoutInflater();
+                    convertView = inflater.inflate(R.layout.task_list_item, parent, false);
+                }
+                Button playPauseButton = (Button) convertView.findViewById(R.id.taskPlayPause);
+                TextView timerTextView = (TextView) convertView.findViewById(R.id.taskTimer);
+                TextView descrTextView = (TextView) convertView.findViewById(R.id.taskDescr);
+                TextView tagTextView   = (TextView) convertView.findViewById(R.id.taskTag);
+
+                playPauseButton.setText(active ? "> " : "||");
+                timerTextView  .setText(timer.toString());
+                descrTextView  .setText(descr);
+                tagTextView    .setText(tag.toString());
+
+                return convertView;
+            }
+        };
+
+        taskListView = (ListView) main.findViewById(R.id.taskListView);
+        taskListView.setAdapter(taskAdapter);
+    }
+}
