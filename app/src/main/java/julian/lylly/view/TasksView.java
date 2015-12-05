845e15214118363ba4a239205509599cefdf0ec4
Julian Tobias Rosemann <s9jnrose@stud.uni-saarland.de>
2015-12-05 21:21:42 +0100

    implement and comment out non-working timer and budgets (/°□°)/︵ ┻━┻

--- app/src/main/java/julian/lylly/view/TasksView.java
+++ app/src/main/java/julian/lylly/view/TasksView.java
@@ -1,7 +1,6 @@
 package julian.lylly.view;
 
 import android.graphics.Typeface;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +14,10 @@ import android.widget.TextView;
 import org.joda.time.Duration;
 
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.List;
 
 import julian.lylly.R;
-import julian.lylly.model.Prospect;
+import julian.lylly.model.Pair;
 import julian.lylly.model.Tag;
 import julian.lylly.model.Task;
 import julian.lylly.model.Util;
@@ -31,18 +29,60 @@ public class TasksView {
 
     private final MainActivity main;
 
-    //TODO: implement budgetListView
     //TODO: implement timer sync
-    //private final ListView budgetListView;
-    //private final ArrayAdapter budgetAdapter;
+    /*private final ListView budgetListView;
+    private final ArrayAdapter budgetAdapter;*/
 
     private final ListView taskListView;
     private final ArrayAdapter taskAdapter;
-    private Button playPauseButton;
 
+    private Handler handler = new Handler();
+    private List<Pair<TextView, Task>> activeVT = new ArrayList<>();
+
+    /*private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            TextView timerView;
+            Duration timer;
+            Task task;
+            for(Pair<TextView, Task> vt : activeVT) {
+                timerView = vt.getFirst();
+                task = vt.getSecond();
+                if (task.isActive()) {
+                    timer = task.evalDurationSum();
+                    timerView.setText(Util.durationToHourMinuteSecondString(timer));
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        }
+    };*/
 
     public TasksView(final MainActivity main) {
         this.main = main;
+
+        //budgets:
+        /*budgetAdapter =
+                new ArrayAdapter<Map<Tag, Pair<Duration, Duration>>>
+                        (main, R.layout.budget_list_item,
+                                (List<Map<Tag, Pair<Duration, Duration>>>) main.getOrganizer().getTodaysBudgets()) {
+//TODO map to list
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        if (convertView == null) {
+                            LayoutInflater inflater = main.getLayoutInflater();
+                            convertView = inflater.inflate(R.layout.budget_list_item, parent, false);
+                        }
+                        //fixme
+                        return convertView;
+                    }
+
+                };
+        budgetListView = (ListView) main.findViewById(R.id.budgetListView);
+        budgetListView.setAdapter(budgetAdapter);*/
+
+
+        //tasks:
         taskAdapter =
                 new ArrayAdapter<Task>(main, R.layout.task_list_item,
                     main.getOrganizer().getFilteredTasks(new ArrayList<Tag>())) {
@@ -63,13 +103,14 @@ public class TasksView {
         taskListView = (ListView) main.findViewById(R.id.taskListView);
         taskListView.setAdapter(taskAdapter);
 
-        AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+        AdapterView.OnItemClickListener onTaskClickListener = new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView parent, View v, int position, long id) {
                 main.goToTaskEdit((Task) parent.getAdapter().getItem(position));
             }
         };
-        taskListView.setOnItemClickListener(mMessageClickedHandler);
+        taskListView.setOnItemClickListener(onTaskClickListener);
 
+        //handler.postDelayed(runnable, 1000);
     }
 
     public void onClickTaskPlayPause(View view) {
@@ -77,8 +118,18 @@ public class TasksView {
         View taskListItem = (View) button.getParent();
         Task task = resolveTaskFromView(taskListItem);
         task.playPause();
+        //updateActiveVT((TextView) taskListItem.findViewById(R.id.taskTimer), task);
         updateTimer(taskListItem);
     }
+
+    /*private void updateActiveVT(TextView timerView, Task task) {
+        Pair<TextView, Task> vt = new Pair<>(timerView, task);
+        if (task.isActive()) {
+            activeVT.add(vt);
+        } else {
+            activeVT.remove(vt);
+        }
+    }*/
 
     public void onClickEditTask(View view) {
         View parent = (View) view.getParent();
@@ -91,6 +142,8 @@ public class TasksView {
         View taskListItem = (View) view.getParent();
         Task task = resolveTaskFromView(taskListItem);
         task.finish();
+
+        //updateActiveVT((TextView) taskListItem.findViewById(R.id.taskTimer), task);
         updateTask(taskListItem, task);
     }
 
@@ -151,3 +204,19 @@ public class TasksView {
         }
     }
 }
+
+/*class VT {
+
+    private final TextView timerView;
+    private final Task task;
+
+    public VT (TextView timerView, Task task) {
+        this.timerView = timerView;
+        this.task = task;
+    }
+
+    void updateTimer() {
+
+    }
+
+}*/
